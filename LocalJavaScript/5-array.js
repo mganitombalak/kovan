e8c94e517346cb7bc6db3f6ac43709499c7669f6
Gokhan <thechief06>
2020-07-19 14:30:38 +0300

    ok

--- LocalJavaScript/5-array.js
+++ LocalJavaScript/5-array.js
@@ -10,8 +10,17 @@ var arr = [
     item => console.log(`value=>${item}`),
     "Hello!"
 ];
-console.log(arr);
-console.log(arr[3]);
-console.log(arr[4]);
-arr[4]("Cikti")
-console.log(Array.isArray(arr));
+// console.log(arr);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// arr[4]("Cikti")
+// console.log(Array.isArray(arr));
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let p = value => console.log(value);
+//DECLARATIVE
+//numbers.forEach(item => console.log(item));
+
+// array Basina item ekledi
+//numbers.unshift(0);
