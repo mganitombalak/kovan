ee63546622e2b7f83ae2e9d23f4347b4324dc950
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-07-21 09:54:59 +0300

    Angular Landin Day

--- JavaScript/XII-iterators-generators.js
+++ JavaScript/XII-iterators-generators.js
@@ -13,33 +13,40 @@ var person = {
 // for (let s of metin){
 //     console.log(s);
 // }
-person[Symbol.iterator] = function* () {
-    yield this.name;
-    yield this.surname;
-    yield this.age 
-}
-// console.log([...person]);
-// for (let field of person) {
-//     console.log(field);
+// person[Symbol.iterator] = function* () {
+//     yield this.name;
+//     yield this.surname;
+//     yield this.age
+// }
+// // console.log([...person]);
+// // for (let field of person) {
+// //     console.log(field);
+// // }
+
+// function* topla(sayi) {
+//     yield sayi + 1;
+//     yield sayi + 2;
+//     yield sayi + 3;
+//     yield sayi + 4;
+//     yield sayi + 5;
+
 // }
 
-function* topla(sayi){
-    yield sayi +1;
-    yield sayi +2;
-    yield sayi +3;
-    yield sayi +4;
-    yield sayi +5;
+// var iterator = topla(1);
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
 
+function* getObjectKeyValuePairs(obj) {
+    const reflectedProps = Reflect.ownKeys(obj);
+    for (const prop of reflectedProps) {
+        yield [prop, obj[prop]];
+    }
 }
 
-var iterator = topla(1);
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.next());
-
-key=IDBCursorWithValue
-name="Gani"
-age=18
+for (let [key, value] of getObjectKeyValuePairs(person)) {
+    console.log(`${key} = ${value}`);
+}
