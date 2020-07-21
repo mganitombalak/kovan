b4575c81dfbaf83f48b46fff46c270d30da7be91
Firat Feroglu <firatferoglu@gmail.com>
2020-07-21 15:26:10 +0300

    lab js duzenleme

--- JavaScript/lab/data/firat/generator.js
+++ JavaScript/lab/data/firat/generator.js
@@ -3,3 +3,14 @@ function* getObjectKeyValuePairs (obj){
         yield {key:key, value:obj.key};
     }  
 }
+
+
+function iterateOverArray(obj) {
+    Object.keys(obj).forEach(key => {
+        console.log(`${key} => Value: ${obj.key} Type: typeof Object.key`);
+    });
+}
+
+function getFunctions(obj){
+   return Object.keys(obj).filter(key => typeof obj.key === 'function').map(key => obj.key);
+}
--- JavaScript/lab/data/lab.js
+++ JavaScript/lab/data/lab.js
@@ -6,13 +6,3 @@ var myObject = {
         this.price=this.price * 1.15;
     }
 };
-
-function iterateOverArray(obj) {
-    Object.keys(obj).forEach(key => {
-        console.log(`${key} => Value: ${obj.key} Type: typeof Object.key`);
-    });
-}
-
-function getFunctions(obj){
-   return Object.keys(obj).filter(key => typeof obj.key === 'function').map(key => obj.key);
-}
