c8497b6798b53508dd48a1787521195a01284b44
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-07-21 14:50:52 +0300

    Directives

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
+import { HoverDirective } from './core/directives/hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AsideMenuComponent
+    AsideMenuComponent,
+    HoverDirective
   ],
   imports: [
     BrowserModule,
