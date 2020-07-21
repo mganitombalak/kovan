03cdef38f31f72ca220e12a426e2c5f4c14c2ad0
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-07-21 15:18:34 +0300

    Directive-II

--- angular/src/app/core/directives/hover.directive.ts
+++ angular/src/app/core/directives/hover.directive.ts
@@ -1,12 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
 })
 export class HoverDirective {
 
-  constructor(private element:ElementRef) { 
-    console.log(element.nativeElement.id);
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener('mouseenter', ['$event'])
+  onMouseEnter(event: Event) {
+    this.renderer.addClass(this.element.nativeElement, 'active');
+    //console.log(`mouse enter:${this.element.nativeElement.id}`);
+  }
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave() {
+    this.renderer.removeClass(this.element.nativeElement, 'active');
+    //console.log(`mouse leave:${this.element.nativeElement.id}`);
   }
 
 }
