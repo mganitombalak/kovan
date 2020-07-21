21e722969ef50596f755cbe73c168e546c939d6a
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-07-21 14:27:58 +0300

    Angular Day II

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideMenuComponent
   ],
   imports: [
     BrowserModule,
