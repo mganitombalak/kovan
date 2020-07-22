65059dedbcfec53a02845f5a181c0d7946ad286a
Mehmet Gani Tombalak <mgani.tombalak@yahoo.com>
2020-07-22 14:10:07 +0300

    Ag-grid

--- angular/src/app/modules/category/category.module.ts
+++ angular/src/app/modules/category/category.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CategoryRoutingModule } from './category-routing.module';
 import { IndexComponent } from './components/index/index.component';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [IndexComponent],
   imports: [
     CommonModule,
-    CategoryRoutingModule
+    CategoryRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class CategoryModule { }
--- angular/src/app/modules/category/components/index/index.component.ts
+++ angular/src/app/modules/category/components/index/index.component.ts
@@ -9,6 +9,18 @@ export class IndexComponent implements OnInit {
 
   constructor() { }
 
+  columnDefs = [
+    { headerName: 'Make', field: 'make', sortable: true },
+    { headerName: 'Model', field: 'model', sortable: true },
+    { headerName: 'Price', field: 'price', sortable: true }
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+];
+
   ngOnInit(): void {
   }
 
